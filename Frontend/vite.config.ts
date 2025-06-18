c00ce686b8d442dd8b6411a70ee80493252724c0
unknown <noguchih@TDK-ZENSY.local>
2025-06-18 10:23:49 +0900

    vite.config.ts設定変更

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-    allowedHosts: ["task-control-app-8arn.onrender.com"],
-  },
+  // server: {
+  //   host: "0.0.0.0",
+  //   port: 5173,
+  //   allowedHosts: ["task-control-app-8arn.onrender.com"],
+  // },
+  base: "/",
 });
