8fd2cad35a4e6ce38a3d6b2fbe77dadd6235bbef
野口博一 <noguchihakuichi@noguchihirokazunoMacBook-Air.local>
2025-07-21 12:00:11 +0900

    週ごとのルーティン初期化修正

--- Backend/src/regularProcessing/initializeRoutines.ts
+++ Backend/src/regularProcessing/initializeRoutines.ts
@@ -1,11 +1,24 @@
 import cron from "node-cron";
 import { RoutineTask } from "../models/routineTask.model";
 
-// 毎日0時にルーティンを初期化する
+// ルーティンを初期化する
 export default function initializeRoutines() {
   try {
+    // 毎日0時に日ごとのルーティンを初期化
     cron.schedule("0 0 15 * * *", async () => {
-      await RoutineTask.find({ status: "done" }).updateMany({
+      await RoutineTask.find({
+        repeatType: "daily",
+        status: "done",
+      }).updateMany({
+        status: "pending",
+      });
+    });
+    // 毎週月曜日の0時に週ごとのルーティンを初期化
+    cron.schedule("0 0 15 * * 7", async () => {
+      await RoutineTask.find({
+        repeatType: "weekly",
+        status: "done",
+      }).updateMany({
         status: "pending",
       });
     });
